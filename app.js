e20a36d2ca7e33e18dd46d8fa00f6bc26f068789
Ignas Kaziukėnas <ignas@ignas.co>
2017-04-05 23:30:35 +0300

    read get parameters and return them back

---
+++ app.js
@@ -0,0 +1,11 @@
+var express = require('express')
+var app = express()
+
+app.get('/*', function (req, res) {
+  var input = req.params[0]
+  res.send(input)
+})
+
+app.listen(8080, function () {
+  console.log('Server listening on port 8080!')
+})
