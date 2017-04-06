28becca9ed357295b1ef71b1c62b2b0cb06b541b
homulvas <ignas@ignas.co>
2017-04-06 23:11:55 +0300

    handle unix time stamp as input

--- server.js
+++ server.js
@@ -2,9 +2,23 @@ var express = require('express')
 var app = express()
 
 app.get('/*', function (req, res) {
+  var result = {unixTimestamp:null, naturalLanguageDate:null}
   var input = req.params[0]
-  res.send(input)
+  if (!isNaN(input)) {
+    var number = +input;
+    if (number >= 0) {
+      result.unixTimestamp = number;
+      result.naturalLanguageDate = timeStampToDate(number)
+    }
+  }
+  res.send(result)
 })
+
+var timeStampToDate = function(num) {
+  var date = new Date(num*1000)
+  var options = { year: 'numeric', month: 'long', day: 'numeric' }
+  return date.toLocaleDateString('en-US', options)
+}
 
 var port = process.env.PORT || 8080
 app.listen(port, function () {
